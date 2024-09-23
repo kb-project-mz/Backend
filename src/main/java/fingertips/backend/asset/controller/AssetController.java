3a928ff066319aede740747c6ecc3cf877182ad5
user.name <95dbswn@hanmail.net>
2024-09-23 11:09:07 +0900

    [SRCUM-137] Fix: ResponseBody 형식 수정
    - ResponseBody 형식 수정
    - DTO 파일 이름만 수정

--- src/main/java/fingertips/backend/asset/controller/AssetController.java
+++ src/main/java/fingertips/backend/asset/controller/AssetController.java
@@ -3,6 +3,7 @@ package fingertips.backend.asset.controller;
 
 import fingertips.backend.asset.dto.AssetDTO;
 import fingertips.backend.asset.service.AssetService;
+import fingertips.backend.exception.dto.JsonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -21,29 +22,29 @@ public class AssetController {
 
     // 사용자가 소유하는 계좌와 카드 정보 가져오기
     @GetMapping("/asset/allAssets/{memberId}")
-    public ResponseEntity<List<AssetDTO>> getAllAssets(@PathVariable("memberId") int memberId) {
+    public ResponseEntity<JsonResponse<List<AssetDTO>>> getAllAssets(@PathVariable("memberId") int memberId) {
         List<AssetDTO> assetList = assetService.getAllAssets(memberId);
-        return ResponseEntity.ok(assetList);
+        return ResponseEntity.ok(JsonResponse.success(assetList));
     }
 
     // 사용자가 연동시킨 계좌와 카드 정보 가져오기
     @GetMapping("/asset/connAssets/{memberId}")
-    public ResponseEntity<List<AssetDTO>> getConnAssets(@PathVariable("memberId") int memberId) {
+    public ResponseEntity<JsonResponse<List<AssetDTO>>> getConnAssets(@PathVariable("memberId") int memberId) {
         List<AssetDTO> assetList = assetService.getConnAssets(memberId);
-        return ResponseEntity.ok(assetList);
+        return ResponseEntity.ok(JsonResponse.success(assetList));
     }
 
     // 카드 연동
     @PostMapping("/card/{cardId}")
-    public ResponseEntity<Void> updateCardStatus(@PathVariable("cardId") int cardId) {
+    public ResponseEntity<JsonResponse<String>> updateCardStatus(@PathVariable("cardId") int cardId) {
         assetService.connCard(cardId);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(JsonResponse.success("Update Success"));
     }
 
     // 계좌 연동
     @PostMapping("/account/{accountId}")
-    public ResponseEntity<Void> updateAccountStatus(@PathVariable("accountId") int accountId) {
+    public ResponseEntity<JsonResponse<String>> updateAccountStatus(@PathVariable("accountId") int accountId) {
         assetService.connAccount(accountId);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(JsonResponse.success("Update Success"));
     }
 }
