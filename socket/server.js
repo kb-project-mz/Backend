1d4e7c071830767a4c50b2e5d374f4cdc5a23a30
woo427 <annejwkim00@naver.com>
2024-09-27 01:19:45 +0900

    Style: socket.io를 위한 server.js파일 재설정

--- socket/server.js
+++ socket/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,27 +12,27 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // vue 클라이언트 포트
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
   },
 });
 
 // 클라이언트가 연결되었을 때의 처리
-io.on("connection", (socket) => {
-  console.log("A user connected");
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-  // balance DB가 업데이트 되었을 때 실행
-  socket.on("balanceUpdate", (memberId, balanceList) => {
+  // balanceUpdate 이벤트 수신 및 브로드캐스트
+  socket.on('balanceUpdate', (memberId, balanceList) => {
     console.log(`Balance updated for member: ${memberId}`);
-    io.emit("balanceUpdate", { memberId, balanceList });
+    io.emit('balanceUpdate', { memberId, balanceList });
   });
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 });
 
 // 서버 포트 설정 및 실행
 server.listen(3001, () => {
-  console.log("Node.js server listening on http://localhost:3001");
+  console.log('Node.js server listening on http://localhost:3001');
 });
