c9c40eec66fc35129db546deb53a9218a1d7a1c5
DYA <sally7410@naver.com>
2024-10-02 10:13:42 +0900

    Fix: RootConfig 파일에 추가

--- src/main/java/fingertips/backend/config/RootConfig.java
+++ src/main/java/fingertips/backend/config/RootConfig.java
@@ -29,6 +29,7 @@ import org.springframework.web.client.RestTemplate;
         "fingertips.backend.admin.mapper",
         "fingertips.backend.asset.mapper",
         "fingertips.backend.challenge.mapper",
+        "fingertips.backend.home.mapper",
         "fingertips.backend.transaction.mapper",
         "fingertips.backend.member.mapper",
         "fingertips.backend.member.sociallogin.mapper",
