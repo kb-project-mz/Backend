cad9b1dfaeff940193ab58c6b4c395aeb68958a3
DYA <sally7410@naver.com>
2024-10-04 01:41:30 +0900

    [SCRUM-287] Feat: 카테고리 별 차트 작업 중 transaction-date 추가

--- src/main/java/fingertips/backend/transaction/dto/CategoryTransactionCountDTO.java
+++ src/main/java/fingertips/backend/transaction/dto/CategoryTransactionCountDTO.java
@@ -15,4 +15,5 @@ public class CategoryTransactionCountDTO {
     private String categoryName;  // 카테고리 이름
     private int transactionCount; // 거래 건수
     private double percentage;    // 카테고리별 거래 비율
+    private String transactionDate;
 }
