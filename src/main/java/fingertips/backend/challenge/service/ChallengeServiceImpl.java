82488ed309b8233ad056bf5ebad84029f0f2bb83
2da00 <86220363+imdayoung@users.noreply.github.com>
2024-10-11 15:34:25 +0900

    [SCRUM-293] Fix: 또래 챌린지 신규 생성 챌린지로 변경

--- src/main/java/fingertips/backend/challenge/service/ChallengeServiceImpl.java
+++ src/main/java/fingertips/backend/challenge/service/ChallengeServiceImpl.java
@@ -67,6 +67,7 @@ public class ChallengeServiceImpl implements ChallengeService {
 
     @Override
     public List<ChallengeDTO> getPeerChallengeList(Integer memberIdx) {
+
         return challengeMapper.getPeerChallengeList(memberIdx);
     }
 
