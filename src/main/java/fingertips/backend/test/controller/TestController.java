499e06405135f650bf55e98cbf20556a9a759b2c
Lee Doha <79359308+leedoha11@users.noreply.github.com>
2024-12-15 22:50:15 +0900

    fix: Test authorization 수정
    - test 컨트롤러에서 memberIdx 없애기

--- src/main/java/fingertips/backend/test/controller/TestController.java
+++ src/main/java/fingertips/backend/test/controller/TestController.java
@@ -2,6 +2,7 @@ package fingertips.backend.test.controller;
 
 
 import fingertips.backend.exception.dto.JsonResponse;
+import fingertips.backend.security.util.JwtProcessor;
 import fingertips.backend.test.dto.*;
 import fingertips.backend.test.service.TestService;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,7 @@ import java.util.*;
 public class TestController {
 
     private final TestService testService;
+    private final JwtProcessor jwtProcessor;
 
     @GetMapping("/questions")
     public ResponseEntity<JsonResponse<List<TestQuestionDTO>>> getAllQuestions() {
@@ -52,8 +54,10 @@ public class TestController {
         return ResponseEntity.ok(JsonResponse.success(results));
     }
 
-    @GetMapping("/survey/{memberIdx}/additional-info")
-    public ResponseEntity<JsonResponse<ForAdditionalSurveyDTO>> getAdditionalSurveyInfo(@PathVariable int memberIdx) {
+    @GetMapping("/survey/additional-info")
+    public ResponseEntity<JsonResponse<ForAdditionalSurveyDTO>> getAdditionalSurveyInfo(@RequestHeader("Authorization") String token) {
+        String accessToken = jwtProcessor.extractToken(token);
+        Integer memberIdx = jwtProcessor.getMemberIdx(accessToken);
         ForAdditionalSurveyDTO results = testService.getAdditionalSurveyInfo(memberIdx);
         return ResponseEntity.ok(JsonResponse.success(results));
     }
