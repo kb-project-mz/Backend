daa8d5818d8542b884af08652df83fc8a86496dd
Lee Doha <79359308+leedoha11@users.noreply.github.com>
2024-12-15 21:42:29 +0900

    fix: account일 시, balance나오게 수정
    - AssetDTO 수정
    - account_balance 테이블 조인하도록 수정

--- src/main/java/fingertips/backend/asset/dto/AssetDTO.java
+++ src/main/java/fingertips/backend/asset/dto/AssetDTO.java
@@ -8,18 +8,11 @@ import lombok.*;
 @AllArgsConstructor
 public class AssetDTO {
 
-//    private Integer financeKind;
-//    private Integer prdtId;
-//    private String financeName;
-//    private String prdtName;
-//    private String image;
-//    private Integer totalAmount;
-//    private Integer connectedStatus;
-//    private Integer balance;
     private Integer assetIdx;
-    private String assetType;         // 'card' 또는 'account'
-    private String bankName;          // 금융사 이름
-    private String assetName;         // 자산 이름
-    private String assetImage;        // 이미지 URL
-    private Integer connectedStatus;  // 연결 상태
+    private String assetType;
+    private String bankName;
+    private String assetName;
+    private String assetImage;
+    private Integer connectedStatus;
+    private Integer balance;
 }
